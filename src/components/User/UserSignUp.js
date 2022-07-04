cdf277943a8414e0e853b3a30fd79e2f617496f4
Sebastian Caicedo <sdrivert@hotmail.com>
2022-07-04 15:26:06 -0500

    Small alert and signup form working

--- src/components/User/UserSignUp.js
+++ src/components/User/UserSignUp.js
@@ -10,30 +10,39 @@ export default class UserSignUp extends Component{
   constructor(){
     super();
     this.state={
-      answer:'Create your Account'
+      answer: true,
+      name: '',
+      email: '',
+      phone: ''
     }
   }
-  
-  
+
+
   render(){
-    const searchUser = async() =>{
-      const response = await axios.get('/usersignup')
-      console.log(response)
-      this.state.answer = response
+    const handleSubmit = async(e) =>{
+      e.preventDefault();
+      var name = this.state.name;
+      var email = this.state.email;
+      var phone = this.state.phone;
+      axios.post('/usersignup', {username: name, useremail: email, userphone: phone}).then(response=>{
+        this.setState({
+          answer: response.data
+        })
+      })
     }
     return(
-      <Form>
-        <h1>{this.state.answer}</h1>
-
+      <Form onClick={handleSubmit}>
+        {!this.state.answer? <div class="alert alert-danger"  role="alert">
+          El usuario que ingresó ya se encuentra registrado
+        </div> : <h1>Create Your Account!</h1>}
         <Form.Group className="mb-3" controlId="formBasicName">
           <Form.Label>Name</Form.Label>
-          <Form.Control type="name" placeholder="Enter your Name" />
-          
+          <Form.Control type="name" placeholder="Enter your Name" onChange={(e)=>{this.setState({name: e.target.value})}}/>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicAddress">
           <Form.Label>Address</Form.Label>
-          <Form.Control type="Address" placeholder="Enter Address" />
+          <Form.Control type="Address" placeholder="Enter Address"/>
           <Form.Text className="text-muted">
             We'll never share your Address with anyone else.
           </Form.Text>
@@ -41,7 +50,7 @@ export default class UserSignUp extends Component{
 
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" placeholder="Enter email" onChange={(e)=>{this.setState({email: e.target.value})}}/>
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -49,9 +58,9 @@ export default class UserSignUp extends Component{
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Phone number</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="Password" onChange={(e)=>{this.setState({phone: e.target.value})}}/>
         </Form.Group>
-        <Button variant="light" type="submit" onClick={searchUser}>
+        <Button variant="light" type="submit">
           Submit
         </Button>
         <Link to = "/user" style={{textDecoration: 'none'}}>
@@ -60,8 +69,6 @@ export default class UserSignUp extends Component{
         <Link to = "/">
           <a className="text-white"> Back home </a>
         </Link>
-
-      
       </Form>
     );
   }
