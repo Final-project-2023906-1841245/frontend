0663294598180c3923628636167056eac18c0deb
Sebastian Caicedo <sdrivert@hotmail.com>
2022-07-16 22:56:30 -0500

    Proxy has been deleted

--- src/components/User/UserSignUp.js
+++ src/components/User/UserSignUp.js
@@ -1,48 +1,70 @@
-import React, {Component} from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import {BrowserRouter as Router, Link} from 'react-router-dom';
-import axios from 'axios'
+import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import axios from "axios";
 
-export default class UserSignUp extends Component{
-
-  constructor(){
+export default class UserSignUp extends Component {
+  constructor() {
     super();
-    this.state={
+    this.state = {
       answer: true,
-      name: '',
-      email: '',
-      phone: '',
-      address: ''
-    }
+      name: "",
+      email: "",
+      phone: "",
+      address: "",
+    };
   }
-  handleSubmit = (e) =>{
+  handleSubmit = (e) => {
     e.preventDefault();
     var name = this.state.name;
     var email = this.state.email;
     var phone = this.state.phone;
     var address = this.state.address;
-    axios.post('/usersignup', {username: name, useremail: email, userphone: phone, useraddress: address}).then(response=>{
-      this.setState({
-        answer: response.data
+    axios
+      .post("localhost:5000/usersignup", {
+        username: name,
+        useremail: email,
+        userphone: phone,
+        useraddress: address,
       })
-    })
-  }
-  render(){
-    return(
+      .then((response) => {
+        this.setState({
+          answer: response.data,
+        });
+      });
+  };
+  render() {
+    return (
       <Form onSubmit={this.handleSubmit}>
-        {!this.state.answer? <div class="alert alert-danger"  role="alert">
-          El usuario que ingresó ya se encuentra registrado
-        </div> : <h1>Create Your Account!</h1>}
+        {!this.state.answer ? (
+          <div class="alert alert-danger" role="alert">
+            El usuario que ingresó ya se encuentra registrado
+          </div>
+        ) : (
+          <h1>Create Your Account!</h1>
+        )}
         <Form.Group className="mb-3" controlId="formBasicName">
           <Form.Label>Name</Form.Label>
-          <Form.Control type="name" placeholder="Enter your Name" onChange={(e)=>{this.setState({name: e.target.value})}}/>
+          <Form.Control
+            type="name"
+            placeholder="Enter your Name"
+            onChange={(e) => {
+              this.setState({ name: e.target.value });
+            }}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicAddress">
           <Form.Label>Address</Form.Label>
-          <Form.Control type="Address" placeholder="Enter Address" onChange={(e)=>{this.setState({address: e.target.value})}}/>
+          <Form.Control
+            type="Address"
+            placeholder="Enter Address"
+            onChange={(e) => {
+              this.setState({ address: e.target.value });
+            }}
+          />
           <Form.Text className="text-muted">
             We'll never share your Address with anyone else.
           </Form.Text>
@@ -50,7 +72,13 @@ export default class UserSignUp extends Component{
 
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" onChange={(e)=>{this.setState({email: e.target.value})}}/>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={(e) => {
+              this.setState({ email: e.target.value });
+            }}
+          />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -58,15 +86,21 @@ export default class UserSignUp extends Component{
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Phone number</Form.Label>
-          <Form.Control type="password" placeholder="Password" onChange={(e)=>{this.setState({phone: e.target.value})}}/>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={(e) => {
+              this.setState({ phone: e.target.value });
+            }}
+          />
         </Form.Group>
         <Button variant="light" type="submit">
           Submit
         </Button>
-        <Link to = "/user" style={{textDecoration: 'none'}}>
-          <a className="text-white"> If you have an Account  </a>,
+        <Link to="/user" style={{ textDecoration: "none" }}>
+          <a className="text-white"> If you have an Account </a>,
         </Link>
-        <Link to = "/">
+        <Link to="/">
           <a className="text-white"> Back home </a>
         </Link>
       </Form>
